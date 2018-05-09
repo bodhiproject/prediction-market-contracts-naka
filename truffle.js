66b3a90f52d1a6ee0c795bdb2a0cfbe1bfbbb06b
Deric Walintukan <deric.walintukan@gmail.com>
2018-05-09 15:27:06 -0700

    Set gas to 4.6m in truffle config

--- truffle.js
+++ truffle.js
@@ -12,8 +12,7 @@ module.exports = {
       port: 8545,
       network_id: 3,
       from: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428",
-      gas: 1000000,
-      gasPrice: 10000000000,
+      gas: 4600000,
     },
   },
 };
