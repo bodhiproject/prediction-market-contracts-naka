51bbc415bd50559c5a1c0ff92cfca9238e58c665
Deric Walintukan <deric.walintukan@gmail.com>
2018-05-15 14:22:03 -0400

    Add mainnet provider info

--- truffle.js
+++ truffle.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+const Web3 = require("web3");
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+
+const web3 = new Web3();
+const mainnetPrivKey = new Buffer(process.env['MEW_MAINNET_KEY'], 'hex');
+const mainnetWallet = Wallet.fromPrivateKey(mainnetPrivKey);
+const mainnetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/jf36VtmNV1eWuSHOMvMT");
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -15,6 +23,12 @@ module.exports = {
       network_id: 3,
       from: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428",
       gas: 4600000,
+    },
+    mainnet: {
+      network_id: 1,
+      provider: mainnetProvider,
+      gas: 4600000,
+      gasPrice: web3.toWei("20", "gwei"),
     },
   },
   solc: {
