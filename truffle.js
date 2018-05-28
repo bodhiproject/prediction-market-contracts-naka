4db585ec538e3735dfe669ca3f2b7040e0967338
Deric Walintukan <deric.walintukan@gmail.com>
2018-05-28 16:09:14 +0700

    Use infura for ropsten

--- truffle.js
+++ truffle.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
-const Web3 = require("web3");
-const WalletProvider = require("truffle-wallet-provider");
+const Web3 = require('web3');
+const WalletProvider = require('truffle-wallet-provider');
 const Wallet = require('ethereumjs-wallet');
 
 const web3 = new Web3();
-const mainnetPrivKey = new Buffer(process.env['MEW_MAINNET_KEY'], 'hex');
-const mainnetWallet = Wallet.fromPrivateKey(mainnetPrivKey);
-const mainnetProvider = new WalletProvider(mainnetWallet, "https://mainnet.infura.io/jf36VtmNV1eWuSHOMvMT");
+
+/*
+* 1. Create .env file locally in the directory root
+* 2. Add private key of the address you will use
+* 3. Reference the .env key here
+*/
+const privKey = new Buffer(process.env['MEW_PRIV_KEY'], 'hex');
+const wallet = Wallet.fromPrivateKey(privKey);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -18,15 +23,13 @@ module.exports = {
       network_id: "*",
     },
     ropsten:  {
-      host: "localhost",
-      port: 8545,
       network_id: 3,
-      from: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428",
+      provider: new WalletProvider(wallet, "https://ropsten.infura.io/jf36VtmNV1eWuSHOMvMT"),
       gas: 4600000,
     },
     mainnet: {
       network_id: 1,
-      provider: mainnetProvider,
+      provider: new WalletProvider(wallet, "https://mainnet.infura.io/jf36VtmNV1eWuSHOMvMT"),
       gas: 4600000,
       gasPrice: web3.toWei("20", "gwei"),
     },
