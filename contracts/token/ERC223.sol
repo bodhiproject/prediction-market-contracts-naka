f7b28858fc46a7b456cfd684ed159c3cd1b8c26f
Deric Walintukan <deric.walintukan@gmail.com>
2018-07-26 15:44:13 +0700

    Refactor comments and version for token contracts

--- contracts/token/ERC223.sol
+++ contracts/token/ERC223.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
-import './ERC20.sol';
+import "./ERC20.sol";
 
 contract ERC223 is ERC20 {
     function transfer(address _to, uint256 _value, bytes _data) public returns (bool success);
