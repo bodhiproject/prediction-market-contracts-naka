36559b62dee8c4c26101e47e84dd283b0ca7c8dd
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-07 11:10:29 +0700

    Add token fallback to event factory

--- contracts/event/EventFactory.sol
+++ contracts/event/EventFactory.sol
@@ -2,17 +2,28 @@ pragma solidity ^0.5.8;
 
 import "./MultipleResultsEvent.sol";
 import "../storage/IConfigManager.sol";
+import "../token/NRC223Receiver.sol";
 
 /// @title Event Factory allows the creation of individual prediction events.
-contract EventFactory {
+contract EventFactory is NRC223Receiver {
     using ByteUtils for bytes32;
+
+    struct EventEscrow {
+        address depositer;
+        uint256 amount;
+    }
 
     uint16 private constant VERSION = 0;
 
     address private _configManager;
-    mapping(bytes32 => MultipleResultsEvent) private events;
+    address private _bodhiTokenAddress;
+    mapping(address => EventEscrow) private _escrows;
+    mapping(bytes32 => MultipleResultsEvent) private _events;
 
     // Events
+    event EscrowDeposited(
+        address indexed newAddress
+    );
     event MultipleResultsEventCreated(
         uint16 indexed version,
         address indexed eventAddress,
@@ -24,9 +35,43 @@ contract EventFactory {
 
     constructor(address configManager) public {
         require(configManager != address(0));
+
         _configManager = configManager;
+        _bodhiTokenAddress = IConfigManager(_configManager).bodhiTokenAddress();
+        assert(_bodhiTokenAddress != address(0));
     }
     
+    /// @dev Handle incoming token transfers needed for creating events.
+    /// @param from Token sender address.
+    /// @param value Amount of tokens.
+    /// @param data The message data. First 4 bytes is function hash & rest is function params.
+    function tokenFallback(
+        address from,
+        uint value,
+        bytes data)
+        external
+    {
+        // Ensure only NBOT can call this method
+        require(msg.sender == _bodhiTokenAddress);
+
+        bytes memory createMultipleResultsEventFunc = hex"8d4c17e1";
+        bytes memory funcHash = data.sliceBytes(0, 4);
+        bytes memory params = data.sliceBytes(4, data.length);
+
+        bytes32 encodedFunc = keccak256(abi.encodePacked(funcHash));
+        if (encodedFunc == keccak256(abi.encodePacked(createMultipleResultsEventFunc))) {
+            (string memory eventName, bytes32[10] eventResults, 
+                uint256 betStartTime, uint256 betEndTime, 
+                uint256 resultSetStartTime, uint256 resultSetEndTime, 
+                address centralizedOracle) = abi.decode(params, (string, 
+                bytes32[10], uint256, uint256, uint256, uint256, address));
+            createMultipleResultsEvent(eventName, eventResults, betStartTime,
+                betEndTime, resultSetStartTime, resultSetEndTime, centralizedOracle);
+        } else {
+            revert("Unhandled function in tokenFallback");
+        }
+    }
+
     function createMultipleResultsEvent(
         string eventName,
         bytes32[10] eventResults,
@@ -35,7 +80,7 @@ contract EventFactory {
         uint256 resultSetStartTime,
         uint256 resultSetEndTime,
         address centralizedOracle)
-        external
+        private
         returns (MultipleResultsEvent)
     {   
         bytes32[11] memory results;
@@ -57,7 +102,7 @@ contract EventFactory {
             name, resultNames, numOfResults, betStartTime, betEndTime, 
             resultSetStartTime, resultSetEndTime);
         // Event should not exist yet
-        require(address(events[eventHash]) == 0);
+        require(address(_events[eventHash]) == 0);
 
         // TODO: NRC223.transfer() -> IronBank -> EventFactory.create
         // IAddressManager(addressManager).transferEscrow(msg.sender);
@@ -66,8 +111,7 @@ contract EventFactory {
             msg.sender, eventName, results, numOfResults, betStartTime,
             betEndTime, resultSetStartTime, resultSetEndTime, centralizedOracle, 
             _configManager);
-        events[eventHash] = mrEvent;
-        IConfigManager(_configManager).addWhitelistContract(address(mrEvent));
+        _events[eventHash] = mrEvent;
 
         emit MultipleResultsEventCreated(VERSION, address(mrEvent), msg.sender, 
             eventName, results);
